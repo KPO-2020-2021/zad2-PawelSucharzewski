ae8c2abf612cfefe06afc2bdbfaade20bc9e224c
PawelSucharzewski <259307@student.pwr.edu.pl>
2021-03-18 15:41:43 +0100

    Funkcje wyrazen

--- src/WyrazenieZesp.cpp
+++ src/WyrazenieZesp.cpp
@@ -5,3 +5,46 @@
  * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
  * w pliku naglowkowym.
  */
+void Wyswietl(WyrazenieZesp  WyrZ)
+{
+    Wyswietl(WyrZ.Arg1);
+
+        switch(WyrZ.Op)
+        {
+        case Op_Dodaj:
+        cout<<"+";
+        break;
+                case Op_Odejmij:
+        cout<<"-";
+        break;
+                case Op_Mnoz:
+        cout<<"*";
+        break;
+                case Op_Dziel:
+        cout<<"/";
+        break;
+        }
+
+     Wyswietl(WyrZ.Arg2);
+}
+
+LZespolona Oblicz(WyrazenieZesp  WyrZ)
+{
+    LZespolona Wynik;
+            switch(WyrZ.Op)
+        {
+        case Op_Dodaj:
+        Wynik= WyrZ.Arg1+WyrZ.Arg2;
+        break;
+                case Op_Odejmij:
+        Wynik= WyrZ.Arg1-WyrZ.Arg2;
+        break;
+                case Op_Mnoz:
+        Wynik= WyrZ.Arg1*WyrZ.Arg2;
+        break;
+                case Op_Dziel:
+        Wynik= WyrZ.Arg1/WyrZ.Arg2;
+        break;
+        }
+return Wynik;
+}
