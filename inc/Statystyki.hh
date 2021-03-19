331af7a75e71fa266cb7c347a52e983558dd5efe
PawelSucharzewski <259307@student.pwr.edu.pl>
2021-03-19 11:04:37 +0100

    Statystyki

--- inc/Statystyki.hh
+++ inc/Statystyki.hh
@@ -1,13 +1,22 @@
 #ifndef STATYSTYKA_HH
 #define STATYSTYKA_HH
-
-
-
+#include <iostream>
 #include "LZespolona.hh"
 #include "WyrazenieZesp.hh"
+
+
 
 /*
  * Tu nalezy zdefiniowac funkcje, ktore definiuja model statystyki 
  */
 
+using namespace std;
+struct statystyka
+{
+    int Poprowane;
+    int Bledny;
+    int Wszystkie;
+};
+
+ostream & operator << (ostream  & Skl1,  const statystyka & Skl2);
 #endif
--- src/Statystyki.cpp
+++ src/Statystyki.cpp
@@ -5,3 +5,9 @@
  * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
  * w pliku naglowkowym.
  */
+ostream & operator << (ostream  & Skl1,  const statystyka & Skl2)
+{
+    Skl1<<"poprawne :"<<Skl2.Poprowane<<endl;
+        Skl1<<"bledne :"<<Skl2.Bledny<<endl;
+            Skl1<<"procent :"<<Skl2.Poprowane*100.0/Skl2.Wszystkie<<endl;
+}
